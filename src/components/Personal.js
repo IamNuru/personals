f46073043a025bb39b44dbcbb9f91b21206da6b1
generous <abdulainurudeentitiaka@gmail.com>
2023-03-20 07:11:30 +0530

    changed

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -20,7 +20,7 @@ const Personal = ({ personal }) => {
     const [shareAnchorEl, setShareAnchorEl] = useState(null);
     const [toggleIconsAnchorEl, setToggleIconsAnchorEl] = useState(null);
 
-    const {width} = useWindowDimensions()
+    const { width } = useWindowDimensions()
 
 
     const sharePopoverOpen = Boolean(shareAnchorEl);
@@ -65,36 +65,40 @@ const Personal = ({ personal }) => {
     return (
         <Box sx={{ mb: 4 }}>
             <label className='label'>{personal.title}</label>
+
             <Card sx={{
                 minHeight: 30, py: 2, backgroundColor: '#daf9f672'
             }} elevation={3}>
+                {
+                    width < 600 &&
+                    <Box sx={{ float: 'right' }}>
+                        <Tooltip title="More">
+                            <MoreVert aria-describedby={ToggleIconsId} onClick={handleClickToggleIcons} className={styles.personal_action_icon} sx={{ fontSize: '2.5rem !important' }} />
+                        </Tooltip>
+                    </Box>
+                }
                 <Box sx={{ pl: 0.2, mt: 1 }}>
                     <Typography variant='h6' sx={{ fontSize: '1.2rem', color: '#0d0c0c', lineHeight: '1.2rem' }}>{personal.content}</Typography>
                 </Box>
+
                 {
-                    width > 600 ?
-                        <Box sx={{ display: 'flex', gap: '0.85rem', float: 'right', pr: { xs: 0.5, sm: 4 } }}>
-                            <Tooltip title="Edit">
-                                <svg onClick={handleOpenEditPersonalModal} className={styles.personal_action_icon} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="m11.25 6c.398 0 .75.352.75.75 0 .414-.336.75-.75.75-1.505 0-7.75 0-7.75 0v12h17v-8.749c0-.414.336-.75.75-.75s.75.336.75.75v9.249c0 .621-.522 1-1 1h-18c-.48 0-1-.379-1-1v-13c0-.481.38-1 1-1zm1.521 9.689 9.012-9.012c.133-.133.217-.329.217-.532 0-.179-.065-.363-.218-.515l-2.423-2.415c-.143-.143-.333-.215-.522-.215s-.378.072-.523.215l-9.027 8.996c-.442 1.371-1.158 3.586-1.264 3.952-.126.433.198.834.572.834.41 0 .696-.099 4.176-1.308zm-2.258-2.392 1.17 1.171c-.704.232-1.274.418-1.729.566zm.968-1.154 7.356-7.331 1.347 1.342-7.346 7.347z" />
-                                </svg>
-                            </Tooltip>
-                            <Tooltip title="Delete">
-                                <Delete onClick={handleOpenDeletePersonalModal} className={styles.personal_action_icon} alt="Delete" />
-                            </Tooltip>
-                            <Tooltip title="Share">
-                                <Share aria-describedby={sharePopoverId} onClick={handleClickShare} className={styles.personal_action_icon} />
-                            </Tooltip>
-                            <Tooltip title={!isCopied ? "Copy" : 'Copied'} variant='soft'>
-                                <ContentCopy onClick={handleCopyText} className={styles.personal_action_icon} />
-                            </Tooltip>
-                        </Box>
-                        :
-                        <Box sx={{ float: 'right' }}>
-                            <Tooltip title="More">
-                                <MoreVert aria-describedby={ToggleIconsId} onClick={handleClickToggleIcons} className={styles.personal_action_icon} sx={{fontSize:'2.5rem !important'}} />
-                            </Tooltip>
-                        </Box>
+                    width > 600 &&
+                    <Box sx={{ display: 'flex', gap: '0.85rem', float: 'right', pr: { xs: 0.5, sm: 4 } }}>
+                        <Tooltip title="Edit">
+                            <svg onClick={handleOpenEditPersonalModal} className={styles.personal_action_icon} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="m11.25 6c.398 0 .75.352.75.75 0 .414-.336.75-.75.75-1.505 0-7.75 0-7.75 0v12h17v-8.749c0-.414.336-.75.75-.75s.75.336.75.75v9.249c0 .621-.522 1-1 1h-18c-.48 0-1-.379-1-1v-13c0-.481.38-1 1-1zm1.521 9.689 9.012-9.012c.133-.133.217-.329.217-.532 0-.179-.065-.363-.218-.515l-2.423-2.415c-.143-.143-.333-.215-.522-.215s-.378.072-.523.215l-9.027 8.996c-.442 1.371-1.158 3.586-1.264 3.952-.126.433.198.834.572.834.41 0 .696-.099 4.176-1.308zm-2.258-2.392 1.17 1.171c-.704.232-1.274.418-1.729.566zm.968-1.154 7.356-7.331 1.347 1.342-7.346 7.347z" />
+                            </svg>
+                        </Tooltip>
+                        <Tooltip title="Delete">
+                            <Delete onClick={handleOpenDeletePersonalModal} className={styles.personal_action_icon} alt="Delete" />
+                        </Tooltip>
+                        <Tooltip title="Share">
+                            <Share aria-describedby={sharePopoverId} onClick={handleClickShare} className={styles.personal_action_icon} />
+                        </Tooltip>
+                        <Tooltip title={!isCopied ? "Copy" : 'Copied'} variant='soft'>
+                            <ContentCopy onClick={handleCopyText} className={styles.personal_action_icon} />
+                        </Tooltip>
+                    </Box>
                 }
 
             </Card>
